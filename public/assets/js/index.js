97fffdd5397155ad20f3625644ced1eb1973e036
Mihaela Valac <mvalac@student.cscc.edu>
2022-01-14 22:19:09 -0500

    Added the routes to the home and notes pages

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -40,6 +40,15 @@ const saveNote = (note) =>
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(note),
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    alert('Error: ' + response.statusText);
+  })
+  .then(postResponse => {
+    console.log(postResponse);
+    alert('Thank you for adding an animal!');
   });
 
 const deleteNote = (id) =>
